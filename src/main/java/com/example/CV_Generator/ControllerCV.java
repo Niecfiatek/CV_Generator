659298b42dc78f44e8aa6242147bf9691e27ee75
NiecfiateQ <bartosz.niedzwiedzki02@gmail.com>
2025-05-20 10:23:21 +0200

    Strona internetowa

---
+++ src/main/java/com/example/CV_Generator/ControllerCV.java
@@ -0,0 +1,25 @@
+package com.example.CV_Generator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class ControllerCV {
+
+    @RequestMapping("/")
+    public String cvForm() {
+        return "cv_form";
+    }
+
+    @PostMapping("/submit")
+    public String submitCv(@RequestParam String name, @RequestParam String email,
+                           @RequestParam String phone, @RequestParam String experience,
+                           @RequestParam String education, @RequestParam String skills,
+                           RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message", "CV zapisane!");
+        return "redirect:/";
+    }
+}
---
+++ src/test/java/com/example/CV_Generator/CvGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CV_Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
